59dc204dd5d698df7289f27a02788b8043bbc7e4
Bojan <bojan.adamovic@madduckcode.com>
2024-01-11 15:29:18 +0100

    Implemented google login

--- appfrontend/src/index.js
+++ appfrontend/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from "react-router-dom";
+import {BrowserRouter as Router} from "react-router-dom";
+import {GoogleOAuthProvider} from "@react-oauth/google";
 
 import './index.css';
 import App from './App';
@@ -8,6 +9,8 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Router>
-    <App />
+    <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_TOKEN}>
+      <App/>
+    </GoogleOAuthProvider>
   </Router>
 );
